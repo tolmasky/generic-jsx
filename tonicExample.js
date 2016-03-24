3841d6f0c7fe037be8324f714fe81e7fd8f22f53
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2016-03-24 11:42:13 -0700

    Update examples.
    Reviewed by @tolmasky.

--- tonicExample.js
+++ tonicExample.js
@@ -2,6 +2,7 @@
 var { curry, from } = require("generic-jsx");
 var BinaryTree = require("generic-jsx/binary-tree");
 
+// BinaryTree is an ES6 class that we can curry using JSX:
 var Division = <BinaryTree value = "/"/>;
 var Addition = <BinaryTree value = "+"/>;
 var Number = <BinaryTree value = { from(0) }/>;
