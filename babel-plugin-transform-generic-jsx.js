b05ca42e63e5a777e6fedb18e4b51e2065f2998f
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2016-09-18 15:11:19 -0700

    Add generic-jsx syntax transform.
    Reviewed by @tolmasky.

---
+++ babel-plugin-transform-generic-jsx.js
@@ -0,0 +1,22 @@
+var curry = "(require(\"generic-jsx\").curry(_=>_))";
+var insertion = require("babylon").parse(curry).program.body[0].expression;
+
+var plugin = function plugin(options)
+{
+    var types = options.types;
+    var visitor = require("babel-helper-builder-react-jsx")(
+    {
+        pre: function(state)
+        {
+            state.args.push(state.tagExpr);
+            state.callee = insertion;
+        }
+    });
+
+    return  {
+                inherits: require("babel-plugin-syntax-jsx"),
+                visitor: visitor
+            };
+}
+
+module.exports = plugin;
