3c478ed49e173d369dd6e30a58e2b2aea419ae15
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2022-08-11 11:52:50 -0400

    New parsing-based

--- generic-jsx/generic-jsx.js
+++ generic-jsx/generic-jsx.js
@@ -1,127 +1,150 @@
-const { inspect } = require("util");
+const given = f => f();
 
-const FromSymbol = Symbol.for("generic-jsx:from");
-const ArgumentsSymbol = Symbol.for("generic-jsx:arguments");
-const UnmappedArgumentsSymbol = Symbol.for("generic-jsx:unmapped-arguments");
-const BaseSymbol = Symbol.for("generic-jsx:base");
+const
+{
+    isArrayPattern,
+    isAssignmentPattern,
+    isIdentifier,
+    isObjectPattern,
+    isObjectProperty,
+    isRestElement
+} = require("@babel/types");
+const { parseExpression } = require("@babel/parser");
 
-const Call = Function.prototype.call.bind(Function.prototype.call);
+const ArrayIsArray = Array.isArray;
 
-const ArrayConcat = Array.prototype.concat;
-const ArraySlice = Array.prototype.slice;
-
-const FunctionToString = Function.prototype.toString;
+const FunctionPrototypeToString = Function.prototype.toString;
 
 const ObjectAssign = Object.assign;
-const ObjectDefineProperty = Object.defineProperty;
+const ObjectGetOwnPropertyNames = Object.getOwnPropertyNames;
+const ObjectHasOwn = Object.hasOwn;
 
-const EmptyObject = Object.create(null);
+const ObjectMerge = (lhs, rhs) => ObjectAssign(lhs, rhs);
+const ObjectDefinePropertyValue =
+    (object, key, value) => Object.defineProperty(object, key, { value });
 
+const fNamed = (name, f) => ObjectDefinePropertyValue(f, "name", name);
 
-function curry(aFunction, newArguments)
+const isPrimitive = value =>
+    value === null || typeof value !== "object" && typeof value !== "function";
+
+const fCached = f => given((
+    cache = new WeakMap(),
+    set = (arguments, value) => (cache.set(arguments[0], value), value),
+    getset = (arguments, deferred) => cache.has(arguments[0]) ?
+        cache.get(arguments[0]) :
+        set(arguments, deferred(...arguments))) =>
+        fNamed(`${f.name}-cached`, (...arguments) => isPrimitive(arguments[0]) ?
+            f(arguments) :
+            getset(arguments, f)));
+
+const BindingOfs = new WeakMap();
+
+const FunctionGetBindingOf = f => BindingOfs.get(f) || false;
+const FunctionGetBaseBindingOf = (f, fPrevious = f) =>
+    f ? FunctionGetBaseBindingOf(FunctionGetBindingOf(f), f) : fPrevious;
+
+Function.getBindingOf = FunctionGetBindingOf;
+
+exports.getBindingOf = FunctionGetBindingOf;
+
+const fParseBindingOf = f => given((
+    bindingOf = FunctionGetBindingOf(f)) =>
+        bindingOf ? fParse(bindingOf) : fParseBindingOf(bindingOf));
+
+const fParse = fCached(f => given((
+    fString = FunctionPrototypeToString.call(f),
+    { type, params } = parseExpression(`(${fString})`),
+    lastParameter = params.length > 0 && params[params.length - 1],
+    restParameter = lastParameter && isRestElement(lastParameter) && lastParameter) =>
+({
+    stringified: fString,
+    toUnmergedArguments: toToArguments(params),
+    isArrowFunction: type === "ArrowFunctionExpression",
+    restParameterName: restParameter && restParameter.argument.name
+})));
+
+const MISSING = { missing: true };
+
+const toToArguments = node =>
+    !node ?
+        () => void(0) :
+    ArrayIsArray(node) ?
+        given((
+            last = node.length >= 1 && node[node.length - 1],
+            rest = last && last.type === "RestElement" && last,
+            nonRest = rest ? node.slice(0, -1) : node,
+            toRestArguments = rest ? toToArguments(rest) : () => [],
+            toNonRestArguments = nonRest.map(toToArguments)) =>
+            named => toNonRestArguments
+                .map(f => f(named))
+                .concat(toRestArguments(named, []))) :
+    isArrayPattern(node) ?
+        toToArguments(node.elements) :
+    isObjectPattern(node) ?
+        given((toArguments = toToArguments(node.properties)) =>
+            named => ObjectAssign({}, ...toArguments(named))) :
+    isObjectProperty(node) ?
+        given((
+            { key, value } = node,
+            computedKey = isIdentifier(key) ? key.name : key.value,
+            toArguments = toToArguments(node.value)) =>
+            named => given((value = toArguments(named, MISSING)) =>
+                value !== MISSING && ({ [computedKey]: value }))) :
+    isIdentifier(node) ?
+        given(({ name } = node) =>
+            (named, fallback) =>
+                ObjectHasOwn(named, name) ? named[name] : fallback) :
+    isAssignmentPattern(node) ?
+        toToArguments(node.left) :
+    isRestElement(node) ?
+        toToArguments(node.argument) :
+    () => void(0);
+
+function fBind(f, attributes, children)
 {
-    const syntacticChildren = Call(ArraySlice, arguments, 2);
-    const previousArguments = aFunction[UnmappedArgumentsSymbol] || false;
-    const children = syntacticChildren.length > 0 ?
-        syntacticChildren :
-        copyChildren(newArguments) ||
-        copyChildren(previousArguments) || [];
+    if (children.length === 0 &&
+        ObjectGetOwnPropertyNames(attributes).length === 0)
+        return f;
 
-    const baseFunction = base(aFunction);
-    const currentArguments = ObjectAssign({ },
-        previousArguments, newArguments, { children });
-    const curried = function _(attributes)
+    const baseBindingOf = FunctionGetBaseBindingOf(f);
+    const
     {
-        const args = resolveFroms(ObjectAssign({ },
-            currentArguments, attributes, arguments));
+        stringified,
+        toUnmergedArguments,
+        isArrowFunction,
+        restParameterName
+    } = fParse(baseBindingOf);
 
-        return Call(baseFunction, this, args);
-    }
+    const mergedAttributes = ObjectAssign(
+        { },
+        f.attributes,
+        attributes,
+        restParameterName && { [restParameterName]: children });
+    const toArguments = arguments =>
+        ObjectMerge(toUnmergedArguments(mergedAttributes), arguments);
 
-    ObjectAssign(curried,
+    const fBound = isArrowFunction ?
+        (...arguments) => baseBindingOf(...toArguments(arguments)) :
+        function (...arguments)
+        {
+            return baseBindingOf.apply(this, toArguments(arguments));
+        };
+
+    BindingOfs.set(fBound, f);
+
+    return ObjectAssign(fNamed(baseBindingOf.name, fBound),
     {
-        [BaseSymbol]: baseFunction,
-        // FIXME: Make this a getter?
-        [ArgumentsSymbol]: resolveFroms(currentArguments),
-        [UnmappedArgumentsSymbol]: currentArguments,
-        toString: curriedToString
+        attributes: mergedAttributes,
+        toString() { return stringified; }
     });
-    ObjectDefineProperty(curried, "name", { value: baseFunction.name });
-
-    return curried;
 }
 
-function curriedToString()
+exports.JSXPragma = function JSXPragma(evalInScope)
 {
-    const baseFunction = base(this);
-    const args = getArguments(this);
-
-    return Call(FunctionToString, baseFunction) + "/* curried: " + inspect(args) + " */";
-}
-
-function copyChildren(args)
-{
-    if (args && args.children)
-        return Call(ArraySlice, args.children, 0);
-
-    return false;
-}
-
-function resolveFroms(args)
-{
-    const keys = Object.keys(args);
-    const hasFrom = keys.some(
-        key => args[key] && args[key][FromSymbol] !== undefined);
-
-    if (!hasFrom)
-        return args;
-
-    const adjusted = { };
-
-    for (const key of keys)
-        adjusted[key] = exhaust(key, args);
-
-    return adjusted;
-}
-
-function exhaust(key, args)
-{
-    const value = args[key];
-
-    if (value && value[FromSymbol] !== undefined)
-        return exhaust(value[FromSymbol], args);
-
-    return value;
-}
-
-function from(aKey)
-{
-    return { [FromSymbol]: aKey };
-}
-
-module.exports.from = from;
-module.exports.curry = curry;
-module.exports.base = base;
-module.exports.getArguments = getArguments;
-module.exports.JSXPragma = JSXPragma;
-
-function getArguments(aFunction)
-{
-    return aFunction[ArgumentsSymbol] || { children:[] };
-}
-
-function base(aFunction)
-{
-    return aFunction[BaseSymbol] || aFunction;
-};
-
-function JSXPragma(evalInScope)
-{
-    return function (_, args)
+    return function JSXPragma(f, attributes, ...children)
     {
-        var children = ArraySlice.call(arguments, 2);
-
-        return curry(evalInScope(_), Object.assign({ }, args, { children: children || [] }));
+        return (f, attributes, ...children) =>
+        fBind(typeof f === "string" ? resolve(f) : f, attributes, children);
     }
 }
-
